ebbadf4004d1f51152eb090d9f3be0f38a2b267d
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2020-09-01 18:24:11 +0900

    Completed write image dowload crawling code from naver

--- AnmalFace/image.py
+++ AnmalFace/image.py
@@ -1,3 +1,4 @@
+import os
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 from urllib.parse import quote_plus
@@ -8,17 +9,23 @@ plusUrl = input('검색어를 입력하세요 : ')
 url = baseUrl + quote_plus(plusUrl)
 html = urlopen(url)
 soup = bs(html, "html.parser")
-img = soup.find_all(class_='_img')
-# img = soup.find_all("a", limit=20)
-# print(img)
+img = soup.find_all(class_='_img', limit=20)
+
+path = "./img/"+plusUrl
+isdir = os.path.isdir(path)
+if not isdir:
+    os.makedirs(path)
+    print("dir created")
+else:
+    print("dir exist")
+
 n = 1
 for i in img:
     imgUrl = i['data-source']
-    # print(imgUrl)
     with urlopen(imgUrl) as f:
-        # w - write b - binary
-        with open('./img/' + plusUrl + str(n)+'.jpg', 'wb') as h:
+        with open('./img/'+plusUrl+"/"+plusUrl+str(n)+'.jpg', 'wb') as h:
             img = f.read()
             h.write(img)
     n += 1
+    f.close()
 print('다운로드 완료')
